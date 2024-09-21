dd570bf2acf791f51751ccf261fefd9eb378f99d
aman satyawani <88000143+satyawaniaman@users.noreply.github.com>
2024-09-21 13:27:40 +0530

    Time: 3 ms (8.35%), Space: 45.6 MB (51.16%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rowSet = new LinkedHashSet<>();
+        Set<Integer> colSet = new LinkedHashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (rowSet.contains(i) || colSet.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
